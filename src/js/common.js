50f62cf4c3a0dd3904712a21c3c1a0087c76aee8
arkad-man <man.arkad.nik@gmail.com>
2019-07-01 16:55:30 +0300

    Added functionality.

--- src/js/common.js
+++ src/js/common.js
@@ -1,1 +1,43 @@
+const inputs = document.getElementsByTagName('input');
 
+let insert = (value) => {
+    let textview = document.getElementById('textview');
+    textview.value += value;
+};
+
+let equal = () => {
+    let exp = document.getElementById('textview');
+    event.preventDefault();
+    if(exp.value) exp.value = eval(exp.value);
+};
+
+let clean = () => {
+    let a = document.getElementById('textview');
+    a.value = a.value = '';
+};
+
+let remove = () => {
+    let textview = document.getElementById('textview');
+    textview.value = textview.value.substring(0, textview.value.length - 1);
+};
+
+document.getElementById('form').onsubmit = () => equal();
+
+for(let i = 0; i < inputs.length; i++){
+    let input = inputs[i];
+
+    if(
+        input.className !== 'textview' &&
+        input.value !== 'C' &&
+        input.value !== '<' &&
+        input.value !== '=' &&
+        input.value !== '%'
+
+    ) input.onclick = () => insert(input.value);
+
+    if(input.value === '=') input.onclick = () => equal();
+
+    if(input.value === 'C') input.onclick = () => clean();
+
+    if(input.value === '<') input.onclick = () => remove();
+};
